158507628cab05cb306fc28b50392d2a1e05d34d
naghachaitanya <34433286+naghachaitanya@users.noreply.github.com>
2018-09-11 10:39:13 +0530

    basic python code
    adding a basic python coding program in git

---
+++ AddingNumbers.py
@@ -0,0 +1,10 @@
+#adding of two numbers
+
+num1 = 10;
+num2 = 20;
+
+#Adding of two numbers
+sum = num1 + num2;
+
+#printing the sum of two numbers
+print("sum of two numbers {0} and {1} is {2}".format(num1,num2,sum));
